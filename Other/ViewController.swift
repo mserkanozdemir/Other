3e0b0017e33132abb8c27d604ef398d9016237b5
Serkan Özdemir <mserkanozdemir@gmail.com>
2018-11-14 11:27:32 +0300

    Initial Commit

---
+++ Other/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Other
+//
+//  Created by Serkan Özdemir on 14.11.2018.
+//  Copyright © 2018 Serkan Özdemir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
